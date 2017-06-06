dabc53706e28cd7ab055ec038c252a1cb85180ac
Fionn Fitzmaurice <iamfionn@github.com>
2017-06-06 19:24:15 +0100

    initial commit

---
+++ dft.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from cmath import exp, sqrt, pi
+
+def fourier(f, s = -1):
+    """Naive FT"""
+    N = len(f)
+    omega = s * 2 * pi * 1j / N
+    x = [sum(f[n] * exp(omega * n * k) for n in range(N)) / sqrt(N) for k in range(N)]
+    return x 
+
+def cround(z):
+    if round(z.imag) == 0:
+        return round(z.real)
+    return round(z.real) + round(z.imag) * 1j
+
+def cprint(f):
+    print(list(map(cround, f)))
+    return True
+
+
+if __name__ == "__main__":
+    f = [0, 1, 0, -1, 0, 1, 0, -1]
+    cprint(f)
+
+    g = fourier(f)
+    cprint(g)
+
+    f = fourier(g, +1)
+    cprint(f)
+
