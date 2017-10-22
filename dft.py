df55bcce07621e4c557712ef703ec9bb48deef16
Fionn Fitzmaurice <iamfionn@github.com>
2017-10-23 01:27:10 +0800

    transformation as method

--- dft.py
+++ dft.py
@@ -2,30 +2,35 @@
 
 from cmath import exp, sqrt, pi
 
-def fourier(f, s = -1):
-    """Naive FT"""
-    N = len(f)
-    omega = s * 2 * pi * 1j / N
-    x = [sum(f[n] * exp(omega * n * k) for n in range(N)) / sqrt(N) for k in range(N)]
-    return x 
+class Vector(list):
 
-def cround(z):
-    if round(z.imag, 9) == 0:
-        return round(z.real, 9)
-    return round(z.real, 9) + round(z.imag, 9) * 1j
+    @staticmethod
+    def _cround(z):
+        n = 8
+        if round(z.imag, n) == 0:
+            return round(z.real, n)
+        return round(z.real, n) + round(z.imag, n) * 1j
 
-def cprint(f):
-    print(list(map(cround, f)))
-    return True
+    def __str__(self):
+        return str(list(map(self._cround, self)))
 
+    def dot(self, other):
+        return sum(u * v for u, v in zip(self, other))
+
+    def dft(self, s = -1):
+        """Naive FT"""
+        N = len(self)
+        omega = s * 2 * pi * 1j / N
+        x = [self.dot([exp(omega * n * k) for n in range(N)]) / sqrt(N) for k in range(N)]
+        return Vector(x)
 
 if __name__ == "__main__":
-    f = [0, 1, 0, -1, 0, 1, 0, -1]
-    cprint(f)
+    f = Vector([0, 1, 0, -1, 0, 1, 0, -1])
+    print(f)
 
-    g = fourier(f)
-    cprint(g)
+    g = f.dft()
+    print(g)
 
-    f = fourier(g, +1)
-    cprint(f)
+    f = g.dft(+1)
+    print(f)
 
